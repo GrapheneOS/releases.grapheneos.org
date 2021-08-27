0f616a1752ef711ae4ff51dbec4ac0897307d122
Daniel Micay <danielmicay@gmail.com>
2021-08-27 01:23:42 -0400

    renumber update servers

--- servers.sh
+++ servers.sh
@@ -1,6 +1,6 @@
 servers=(
     0.releases.grapheneos.org
     1.releases.grapheneos.org
-    2.releases.grapheneos.org
-    3.releases.grapheneos.org
+    4.releases.grapheneos.org
+    5.releases.grapheneos.org
 )
