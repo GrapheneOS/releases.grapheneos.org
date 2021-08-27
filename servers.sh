ae02026cafdd5c2a376ea0779757c18ce6886b38
Daniel Micay <danielmicay@gmail.com>
2021-08-27 05:26:36 -0400

    add new North America update servers

--- servers.sh
+++ servers.sh
@@ -1,6 +1,8 @@
 servers=(
     0.releases.grapheneos.org
     1.releases.grapheneos.org
+    2.releases.grapheneos.org
+    3.releases.grapheneos.org
     4.releases.grapheneos.org
     5.releases.grapheneos.org
 )
